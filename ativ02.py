5b7a7c360f124e7d09a461e3c45bd7abecd85d81
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-04 15:31:10 -0300

    formula

---
+++ ativ02.py
@@ -0,0 +1,8 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+c=float(input("digite"))
+f=c* 9/5 + 32
+
+print(f"a convesao é:{f}")
+5
+
+      
